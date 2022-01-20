85fa438984a8430195a3be7fc48a46078778b936
Niranjan Rajendran <me@niranjan.io>
2022-01-20 14:34:04 +0530

    Add kubeconfig module

---
+++ eks/kubectl/kubeconfig/main.tf
@@ -0,0 +1,50 @@
+variable "cluster_name" {
+  type        = string
+  description = "Name of the cluster"
+}
+
+data "aws_eks_cluster" "this" {
+  name = var.cluster_name
+}
+
+data "aws_eks_cluster_auth" "this" {
+  name = var.cluster_name
+}
+
+locals {
+  kubeconfig = yamlencode({
+    apiVersion      = "v1"
+    kind            = "Config"
+    current-context = "terraform"
+    clusters        = [
+      {
+        name    = data.aws_eks_cluster.this.name
+        cluster = {
+          certificate-authority-data = data.aws_eks_cluster.this[0].certificate_authority[0].data
+          server                     = data.aws_eks_cluster.this.endpoint
+        }
+      }
+    ]
+    contexts        = [
+      {
+        name    = "terraform"
+        context = {
+          cluster = data.aws_eks_cluster.this.name
+          user    = "terraform"
+        }
+      }
+    ]
+    users           = [
+      {
+        name = "terraform"
+        user = {
+          token = data.aws_eks_cluster_auth.this.token
+        }
+      }
+    ]
+  })
+}
+
+output "kubeconfig" {
+  value = local.kubeconfig
+}
