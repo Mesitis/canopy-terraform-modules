bb5dd1a9268ee317b8ed6bf6bb4d6805523e4869
Niranjan Rajendran <me@niranjan.io>
2022-10-31 23:26:39 +0530

    Use less restrictive version ranges

--- eks/workloads/versions.tf
+++ eks/workloads/versions.tf
@@ -2,23 +2,23 @@ terraform {
   required_providers {
     helm = {
       source = "hashicorp/helm"
-      version = "~> 2.3"
+      version = ">= 2.3"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "~> 2.5"
+      version = ">= 2.5"
     }
     local = {
       source = "hashicorp/local"
-      version = "~> 2.1"
+      version = ">= 2.1"
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 3.1"
+      version = ">= 3.1"
     }
     template = {
       source = "hashicorp/template"
-      version = "~> 2.2"
+      version = ">= 2.2"
     }
   }
   required_version = ">= 0.13"
